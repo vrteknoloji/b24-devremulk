7b5fa63e32037b7b82839e689b26e8a91d075239
vurmazm <57285738+vurmazm@users.noreply.github.com>
2026-05-13 23:40:24 +0300

    fix: install.html path düzeltildi

--- netlify/functions/install.js
+++ netlify/functions/install.js
@@ -2,7 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async function(event, context) {
-  const html = fs.readFileSync(path.join(__dirname, '../../install.html'), 'utf8');
+  // Netlify functions dizini: netlify/functions/install.js
+  // install.html: proje kökünde
+  const htmlPath = path.join(__dirname, '..', '..', 'install.html');
+  const html = fs.readFileSync(htmlPath, 'utf8');
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'text/html; charset=utf-8' },
