c23b51b12e96c3833572376f5bf8b0383adee63a
vurmazm <57285738+vurmazm@users.noreply.github.com>
2026-05-13 23:39:25 +0300

    Netlify function: POST desteği install.html için

---
+++ netlify/functions/install.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async function(event, context) {
+  const html = fs.readFileSync(path.join(__dirname, '../../install.html'), 'utf8');
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'text/html; charset=utf-8' },
+    body: html
+  };
+};
