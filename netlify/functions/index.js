803582b4d7fbb2f780b199541a7a7c4dea1d3a4e
vurmazm <57285738+vurmazm@users.noreply.github.com>
2026-05-13 23:51:44 +0300

    fix: process.cwd() ile path düzeltildi

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async function(event, context) {
-  const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+  const html = fs.readFileSync(path.join(process.cwd(), 'index.html'), 'utf8');
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'text/html; charset=utf-8' },
--- netlify/functions/install.js
+++ netlify/functions/install.js
@@ -118,7 +118,7 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async function(event, context) {
-  const html = fs.readFileSync(path.join(__dirname, 'install.html'), 'utf8');
+  const html = fs.readFileSync(path.join(process.cwd(), 'install.html'), 'utf8');
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'text/html; charset=utf-8' },
